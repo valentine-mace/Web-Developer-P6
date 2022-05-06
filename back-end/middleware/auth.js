cbcfd4f49eeba1e6e98431c5aef3961079e02f97
valentine-mace <valentine.mace@hotmail.com>
2022-05-06 15:07:39 +0200

    on enlève comm inutiles

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,18 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-//nous insérons tout à l'intérieur d'un bloc try...catch ;
-//nous extrayons le token du header Authorization de la requête entrante.
-//N'oubliez pas qu'il contiendra également le mot-clé Bearer .
-//Nous utilisons donc la fonction split pour récupérer tout après l'espace dans le header. 
-//Les erreurs générées ici s'afficheront dans le bloc catch ;
-//nous utilisons ensuite la fonction verify pour décoder notre token. 
-//Si celui-ci n'est pas valide, une erreur sera générée ;
-//nous extrayons l'ID utilisateur de notre token ;
-//si la demande contient un ID utilisateur, nous le comparons à celui extrait du token. 
-//S'ils sont différents, nous générons une erreur ;
-//dans le cas contraire, tout fonctionne, et notre utilisateur est authentifié. 
-//Nous passons l'exécution à l'aide de la fonction next() 
-
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
